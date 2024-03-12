7e162bb973fc5a265f749d7f866f427bc8abda4b
praisephs <uyioboite@gmail.com>
2024-03-12 15:38:11 +0100

    Added deployment of virtual network to the config file

--- main.tf
+++ main.tf
@@ -18,3 +18,10 @@ resource "azurerm_resource_group" "this" {
   name     = "RG-uyi"
   location = "eastus2"
 }
+
+resource "azurerm_virtual_network" "this" {
+  name                = "vnet-uyi"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
