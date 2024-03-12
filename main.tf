fda31ec047899ae78ac8780ca534cf6d723d561a
praisephs <uyioboite@gmail.com>
2024-03-12 16:09:01 +0100

    Added deployment of VM to the config file

--- main.tf
+++ main.tf
@@ -45,3 +45,27 @@ resource "azurerm_network_interface" "this" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+
+resource "azurerm_linux_virtual_machine" "this" {
+  name                = "vm-uyi"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "AdminUser1234"
+  network_interface_ids = [
+    azurerm_network_interface.this.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
