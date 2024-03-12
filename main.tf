d33011b8a2ca4ebd62c078e338ffcb5b1122672f
praisephs <uyioboite@gmail.com>
2024-03-12 15:49:18 +0100

    Added deployment of subnet to the config file

--- main.tf
+++ main.tf
@@ -24,4 +24,12 @@ resource "azurerm_virtual_network" "this" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
+  depends_on = [ azurerm_resource_group.this ]
+}
+
+resource "azurerm_subnet" "this" {
+  name                 = "subnet-uyi"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
