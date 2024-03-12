5c1d9528cbc46ce01314b5e3499fa45c123bf966
praisephs <uyioboite@gmail.com>
2024-03-12 15:58:18 +0100

    Added deployment of network interface to the config file

--- main.tf
+++ main.tf
@@ -33,3 +33,15 @@ resource "azurerm_subnet" "this" {
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
+resource "azurerm_network_interface" "this" {
+  name                = "nic-uyi"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.this.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
