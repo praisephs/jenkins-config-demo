d2dd45149cfb36ffbde94841ef0c27c429abbbde
praisephs <uyioboite@gmail.com>
2024-03-15 21:44:20 +0100

    Updated the config file

--- main.tf
+++ main.tf
@@ -19,13 +19,13 @@ resource "azurerm_resource_group" "this" {
   location = "eastus2"
 }
 
-resource "azurerm_virtual_network" "this" {
-  name                = "vnet-uyi"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  depends_on = [ azurerm_resource_group.this ]
-}
+# resource "azurerm_virtual_network" "this" {
+#   name                = "vnet-uyi"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.this.location
+#   resource_group_name = azurerm_resource_group.this.name
+#   depends_on = [ azurerm_resource_group.this ]
+# }
 
 # resource "azurerm_subnet" "this" {
 #   name                 = "subnet-uyi"
